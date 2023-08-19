070bf49dd6381ec234130424bfe7d5562fc3654f
homophobic <126523110+02-Dcs@users.noreply.github.com>
2023-08-19 21:44:18 +0100

    Create Creator.lua

---
+++ Creator.lua
@@ -0,0 +1,29 @@
+local Root = script.parent
+
+local Creator = {}
+
+function Creator.New(Name, Properties, Children)
+	local Object = Instance.new(Name)
+
+	-- Default properties
+	for Name, Value in next, Creator.DefaultProperties[Name] or {} do
+		Object[Name] = Value
+	end
+
+	-- Properties
+	for Name, Value in next, Properties or {} do
+		if Name ~= "ThemeTag" then
+			Object[Name] = Value
+		end
+	end
+
+	-- Children
+	for _, Child in next, Children or {} do
+		Child.Parent = Object
+	end
+
+	ApplyCustomProps(Object, Properties)
+	return Object
+end
+
+return Creator
