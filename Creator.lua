4cae1e6150c81418399c7080a97ebf40bbe30044
homophobic <126523110+02-Dcs@users.noreply.github.com>
2023-08-19 22:06:58 +0100

    Update Creator.lua

--- Creator.lua
+++ Creator.lua
@@ -1,4 +1,4 @@
-local Root = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChildOfClass("Frame")
+local Root = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChildOfClass("ScreenGui"):FindFirstChildOfClass("Frame")
 
 local Creator = {}
 
