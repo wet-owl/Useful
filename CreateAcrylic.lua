e02bbef5192bff79b0150db312136cd943b734cc
homophobic <126523110+02-Dcs@users.noreply.github.com>
2023-08-19 22:05:53 +0100

    Update CreateAcrylic.lua

--- CreateAcrylic.lua
+++ CreateAcrylic.lua
@@ -1,4 +1,4 @@
-local Root = G2L["1"]
+local Root = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChildOfClass("Frame")
 local Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/02-Dcs/Test/main/Creator.lua"))()
 
 local function createAcrylic()
