ac6e4a2a2e161f42bb4fbb9463cccf67c318fbfc
homophobic <126523110+02-Dcs@users.noreply.github.com>
2023-09-03 16:42:57 +0100

    Create ls.lua

---
+++ ls.lua
@@ -0,0 +1,24 @@
+shared.exploit = nil
+local formatted = return
+formatted = math.huge
+math.huge = return
+shared.temp1 = return
+local return = print
+local that = false
+that = true
+shared.exploits = that
+local Other = game:Shutdown()
+shared.temp = Other
+Other = nil
+local faketable = {40824}
+shared.exploits = shared.temp1
+other = shared.Temp
+local tabletoreturn = {85823}
+shared.Temp = nil
+local empty = {}
+if that then that = 2.5921 end
+if that == 2.5921 then local returned = false end
+if shared.returntheresult == true then Other() end
+for a, b in pairs(faketable) do table.insert(empty, b) end
+for c, d in pairs(tabletoreturn) do table.insert(empty, d) end
+shared.temp1 empty
